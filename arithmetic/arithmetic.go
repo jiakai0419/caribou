6a93c8f54b4994c2e187b25e47416b8539ee352e
KaiJia <jiakai0419@gmail.com>
2017-04-27 14:00:59 +0800

    add MaxI64S & MinI64S
    Change-Id: I24debbfaba9af92ca2719dc1a497518b6bdc7b9c

--- arithmetic/arithmetic.go
+++ arithmetic/arithmetic.go
@@ -1,5 +1,10 @@
 package arithmetic
 
+const (
+	UPPER_BOUND_I64 = 9223372036854775807  // 2^63 - 1
+	LOWER_BOUND_I64 = -9223372036854775808 // - 2^63
+)
+
 func MaxI64(a, b int64) int64 {
 	if a > b {
 		return a
@@ -12,4 +17,24 @@ func MinI64(a, b int64) int64 {
 		return a
 	}
 	return b
+}
+
+func MaxI64S(nums ...int64) int64 {
+	var mx int64 = LOWER_BOUND_I64
+	for _, num := range nums {
+		if num > mx {
+			mx = num
+		}
+	}
+	return mx
+}
+
+func MinI64S(nums ...int64) int64 {
+	var mn int64 = UPPER_BOUND_I64
+	for _, num := range nums {
+		if num < mn {
+			mn = num
+		}
+	}
+	return mn
 }
